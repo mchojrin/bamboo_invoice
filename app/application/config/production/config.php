0362cb23bde581c54ea76fe6d55544c2fd443bfc
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2024-02-21 10:54:11 +0100

    Creating specicif config files for dev/prod

---
+++ app/application/config/production/config.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your Code Igniter root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|	http://www.your-site.com/
+|
+*/
+$config['base_url']	= 'https://bamboo_prod';
+// This should be a full address, with a slash.  For example, here's what
+// I use on bambooinvoice.net:
+//$config['base_url']	= 'http://localhost/bamboo';
+
+/* End of file config.php */
+/* Location: ./system/application/config/config.php */
